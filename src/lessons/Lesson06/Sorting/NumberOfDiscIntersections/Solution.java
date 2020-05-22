a43fa539138c128d86d276f5528b23d5816d7c8f
Diogo Lins <diogo.lins@gmail.com>
2020-05-22 13:39:03 -0300

    commit

--- src/lessons/Lesson06/Sorting/NumberOfDiscIntersections/Solution.java
+++ src/lessons/Lesson06/Sorting/NumberOfDiscIntersections/Solution.java
@@ -35,4 +35,10 @@ class Solution {
         return inter;
     }
 
+   public static void main(String[] args) {
+	   Solution solution = new Solution();
+	   
+	   System.out.println(solution.solution(new int[] {1, 5, 2, 1, 4, 0}));
+   }
+   
 }
